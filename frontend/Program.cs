61867b57b32c35eae00292a506ea45954b7023f6
fukicycle <lego.sato.4135@gmail.com>
2024-02-27 08:52:13 +0900

    Add settings

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -7,7 +7,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7186") });
-builder.Services.AddHttpClient("Debug", httpClient =>
+builder.Services.AddHttpClient("Dev", httpClient =>
 {
     httpClient.BaseAddress = new Uri("https://localhost:7186");
 });
