62cc2892daea69321988d8474eef7780261183d0
fukicycle <lego.sato.4135@gmail.com>
2024-02-27 08:54:38 +0900

    Add goal controller

---
+++ backend/Controllers/GoalController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("/api/v1/goals")]
+    public class GoalController : ControllerBase
+    {
+        private readonly DB _db;
+        public GoalController(DB db)
+        {
+            _db = db;
+        }
+    }
+}
