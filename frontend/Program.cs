c31447c58b1ec1178cf4aedc48a9c82e4bc862e8
fukicycle <lego.sato.4135@gmail.com>
2024-02-27 08:53:11 +0900

    update client factory

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -1,17 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using frontend;
+using Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7186") });
-builder.Services.AddHttpClient("Dev", httpClient =>
+builder.Services.AddHttpClient(nameof(ApplicationMode.Dev), httpClient =>
 {
     httpClient.BaseAddress = new Uri("https://localhost:7186");
 });
-builder.Services.AddHttpClient("Prod", httpClient =>
+builder.Services.AddHttpClient(nameof(ApplicationMode.Prod), httpClient =>
 {
     httpClient.BaseAddress = new Uri("https://www.sato-home.mydns.jp:9444");
 });
