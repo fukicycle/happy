cac60e07d633f23df15e8682cb1f917813ba1ac2
fukicycle <lego.sato.4135@gmail.com>
2024-02-27 08:39:57 +0900

    add db context

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,6 @@
+using backend;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DB>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DB")));
 
 var app = builder.Build();
 
