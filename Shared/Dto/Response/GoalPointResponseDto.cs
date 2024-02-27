31940db38a2198eb4fb473ae8c3c98cfdd5ac386
fukicycle <lego.sato.4135@gmail.com>
2024-02-27 09:00:12 +0900

    Add response dto

---
+++ Shared/Dto/Response/GoalPointResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dto.Response
+{
+    public class GoalPointResponseDto
+    {
+        public GoalPointResponseDto(Guid guid, string content, int point)
+        {
+            Guid = guid;
+            Content = content;
+            Point = point;
+        }
+        public Guid Guid { get; }
+        public string Content { get; }
+        public int Point { get; }
+    }
+}
---
+++ Shared/Dto/Response/GoalResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dto.Response
+{
+    public class GoalResponseDto
+    {
+        public GoalResponseDto(Guid guid, string email, int targetYear, int targetMonth)
+        {
+            Guid = guid;
+            Email = email;
+            TargetYearMonth = $"{targetYear:0000}年{targetMonth:00}月";
+        }
+        public Guid Guid { get; }
+        public string Email { get; }
+        public string TargetYearMonth { get; set; }
+    }
+}
