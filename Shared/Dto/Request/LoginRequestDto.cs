4aae0d4777c9328c06343304e13926b795fefcba
fukicycle <lego.sato.4135@gmail.com>
2024-02-27 13:11:17 +0900

    add response and request dto

---
+++ Shared/Dto/Request/LoginRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dto.Request
+{
+    public class LoginRequestDto
+    {
+        public LoginRequestDto(string email)
+        {
+            Email = email;
+        }
+
+        public string Email { get; }
+    }
+}
