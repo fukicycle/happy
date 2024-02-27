fbb071457b5b645516a5ffb41a5caeefcedeb5ef
fukicycle <lego.sato.4135@gmail.com>
2024-02-27 13:12:07 +0900

    update controller

--- backend/Controllers/LoginController.cs
+++ backend/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
 ﻿using backend.Services.Interface;
 using Microsoft.AspNetCore.Mvc;
+using Shared.Dto.Request;
+using Shared.Dto.Response;
 
 namespace backend.Controllers
 {
@@ -12,12 +14,12 @@ namespace backend.Controllers
             _authenticationService = authenticationService;
         }
         [HttpPost]
-        public IActionResult Login([FromBody] string email)
+        public IActionResult Login(LoginRequestDto loginRequestDto)
         {
-            if (_authenticationService.Authentication(email))
+            if (_authenticationService.Authentication(loginRequestDto.Email))
             {
-                string token = _authenticationService.GenerateAccessToken(email);
-                return Ok(token);
+                string token = _authenticationService.GenerateAccessToken(loginRequestDto.Email);
+                return Ok(new LoginResponseDto(token));
             }
             return Unauthorized();
         }
