8f8016bdb1e58dd203ba3c82e5d3f02ad126f2fd
fukicycle <lego.sato.4135@gmail.com>
2024-02-29 13:41:15 +0900

    add dialog

---
+++ frontend/Components/Dialog.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace frontend.Components
+{
+    public partial class Dialog
+    {
+        [Parameter]
+        public string Title { get; set; } = string.Empty;
+        [Parameter]
+        public string Message { get; set; } = string.Empty;
+
+        private string _displayChangeStyle = "display: none;";
+
+        public void Close()
+        {
+            _displayChangeStyle = "display: none;";
+        }
+
+        public void Open()
+        {
+            _displayChangeStyle = "display: block;";
+        }
+    }
+}
