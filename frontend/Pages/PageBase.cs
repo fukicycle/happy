4c43b05a098d8c32ee4b14c7010d5f13d0b7ee50
fukicycle <lego.sato.4135@gmail.com>
2024-02-29 13:20:33 +0900

    Add page base

---
+++ frontend/Pages/PageBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace frontend.Pages
+{
+    public class PageBase : ComponentBase, IDisposable
+    {
+        private readonly IStateContainer _stateContainer;
+        public PageBase(IStateContainer stateContainer)
+        {
+            _stateContainer = stateContainer;
+        }
+
+        protected override void OnInitialized()
+        {
+            _stateContainer.OnStateChanged += StateHasChanged;
+        }
+        public void Dispose()
+        {
+            _stateContainer.OnStateChanged -= StateHasChanged;
+        }
+    }
+}
