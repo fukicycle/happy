077aaa26f82bb905da7410e4e0f780831f94b26e
fukicycle <lego.sato.4135@gmail.com>
2024-03-04 14:32:50 +0900

    追加ページのUIを実装

---
+++ frontend/Pages/AddGoal.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace Happy.frontend.Pages
+{
+    public partial class AddGoal
+    {
+        private DateTime? _periodStartDate = null;
+        private DateTime? _periodEndDate = null;
+
+        protected override void OnInitialized()
+        {
+            //TODO サービスへの切り出し(?)
+            int idx = typeof(DayOfWeek).GetEnumNames().ToList().IndexOf(DateTime.Today.DayOfWeek.ToString());
+            _periodStartDate = DateTime.Today.AddDays(-idx);
+            _periodEndDate = _periodStartDate.Value.AddDays(6);
+        }
+
+        private string GetDisplayTextFromDateTime(DateTime? date)
+        {
+            if (date == null)
+            {
+                throw new ArgumentNullException($"Parameter is null: {nameof(date)}");
+            }
+            return date.Value.ToString("MM/dd(ddd)");
+        }
+    }
+}
