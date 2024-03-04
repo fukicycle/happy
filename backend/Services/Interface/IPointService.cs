984a49d6be3b0125dc295a61d23afeefc78218f6
fukicycle <lego.sato.4135@gmail.com>
2024-03-04 11:58:08 +0900

    add return user points method

--- backend/Services/Interface/IPointService.cs
+++ backend/Services/Interface/IPointService.cs
@@ -1,7 +1,10 @@
-﻿namespace Happy.backend.Services.Interface
+﻿using Happy.Shared.Dto.Response;
+
+namespace Happy.backend.Services.Interface
 {
     public interface IPointService
     {
         void AddPoint(string email, Guid goalPointGuid);
+        UserPointResponseDto GetUserPointResponseDtoByEmail(string email);
     }
 }
--- backend/Services/PointService.cs
+++ backend/Services/PointService.cs
@@ -1,5 +1,7 @@
 ﻿using Happy.backend.Services.Interface;
+using Happy.Shared.Dto.Response;
 using Happy.Shared.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Happy.backend.Services
 {
@@ -32,6 +34,27 @@ namespace Happy.backend.Services
                 };
                 _db.PointHistories.Add(pointHistory);
                 _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+        }
+
+        public UserPointResponseDto GetUserPointResponseDtoByEmail(string email)
+        {
+            try
+            {
+                Member? member = _db.Members.Find(email);
+                if (member == null)
+                {
+                    throw new Exception($"No such user: {email}");
+                }
+                IList<PointHistory> pointHistories = _db.PointHistories.Include(pointHistory => pointHistory.GoalPoint).Where(pointHistory => pointHistory.Email == email).ToList();
+                int yesterdayPoint = pointHistories.Where(pointHistory => pointHistory.Date == DateTime.Today.AddDays(-1)).Sum(pointHistory => pointHistory.GoalPoint.Point);
+                int totalPoint = pointHistories.Sum(pointHistory => pointHistory.GoalPoint.Point);
+                return new UserPointResponseDto(yesterdayPoint, totalPoint);
             }
             catch (Exception ex)
             {
