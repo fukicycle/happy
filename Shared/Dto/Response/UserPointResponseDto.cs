e73ea20fbb1d2b9440bb01f4a3955f5274d0c250
fukicycle <lego.sato.4135@gmail.com>
2024-03-04 11:58:14 +0900

    add response dto

---
+++ Shared/Dto/Response/UserPointResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happy.Shared.Dto.Response
+{
+    public sealed class UserPointResponseDto
+    {
+        public UserPointResponseDto(int yesterdayPoint, int totalPoint)
+        {
+            YesterdayPoint = yesterdayPoint;
+            TotalPoint = totalPoint;
+
+        }
+        public int YesterdayPoint { get; }
+        public int TotalPoint { get; }
+    }
+}
