28163889be6716244a333b321cf0a2023198a047
fukicycle <lego.sato.4135@gmail.com>
2024-03-04 12:38:19 +0900

    Add value object for point

---
+++ Shared/ValueObjects/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happy.Shared.ValueObjects
+{
+    public sealed class Point
+    {
+        private readonly string UnitName = "pt";
+        public Point(int value)
+        {
+            Value = value;
+            DisplayText = $"{value}{UnitName}";
+        }
+        public int Value { get; }
+        public string DisplayText { get; }
+    }
+}
