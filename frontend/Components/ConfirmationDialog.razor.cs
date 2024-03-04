7438110bf1fc776c805dc6622e4ae051212774fc
fukicycle <lego.sato.4135@gmail.com>
2024-03-04 11:38:34 +0900

    Add confirmation dialog

---
+++ frontend/Components/ConfirmationDialog.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Happy.frontend.Components
+{
+    public partial class ConfirmationDialog
+    {
+        [Parameter]
+        public string Title { get; set; } = string.Empty;
+        [Parameter]
+        public string Message { get; set; } = string.Empty;
+
+        [Parameter]
+        public EventCallback CancelButtoOnClick { get; set; }
+
+        private string _displayChangeStyle = "display: none;";
+
+        public void OkClose()
+        {
+            Title = string.Empty;
+            Message = string.Empty;
+            _displayChangeStyle = "display: none;";
+        }
+
+        public async Task CacnelClose()
+        {
+            await CancelButtoOnClick.InvokeAsync();
+            Title = string.Empty;
+            Message = string.Empty;
+            _displayChangeStyle = "display: none;";
+        }
+
+        public void Open()
+        {
+            _displayChangeStyle = "display: flex;";
+        }
+    }
+}
