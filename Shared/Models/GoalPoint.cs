4655488e1e349fee76484b31c25c2cf62c6a735b
Fuki Sato <lego.sato.4135@gmail.com>
2024-03-04 23:06:57 +0900

    update models

--- Shared/Models/GoalPoint.cs
+++ Shared/Models/GoalPoint.cs
@@ -5,11 +5,17 @@ namespace Happy.Shared.Models
 {
     public partial class GoalPoint
     {
+        public GoalPoint()
+        {
+            PointHistories = new HashSet<PointHistory>();
+        }
         public Guid Guid { get; set; }
         public Guid GoalGuid { get; set; }
         public string Content { get; set; } = null!;
         public int Point { get; set; }
 
         public virtual Goal GoalGu { get; set; } = null!;
+        public virtual ICollection<PointHistory> PointHistories { get; set; }
+
     }
 }
