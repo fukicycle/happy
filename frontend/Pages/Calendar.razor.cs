62d9ae34e4fc0e95c39ff94c29820bf18d383963
fukicycle <lego.sato.4135@gmail.com>
2024-03-04 16:48:52 +0900

    Implements calendar page

---
+++ frontend/Pages/Calendar.razor.cs
@@ -0,0 +1,44 @@
+﻿using Happy.frontend.Models;
+
+namespace Happy.frontend.Pages
+{
+    public partial class Calendar : PageBase
+    {
+        private IEnumerable<CalendarCellItem> _calendarCellItems = Enumerable.Empty<CalendarCellItem>();
+        private DateTime _date = DateTime.Today;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await RefreshCalendar();
+        }
+
+        private async Task PrevButtonOnClick()
+        {
+            _date = _date.AddMonths(-1);
+            await RefreshCalendar();
+        }
+
+        private async Task NextButtonOnClick()
+        {
+            _date = _date.AddMonths(1);
+            await RefreshCalendar();
+        }
+
+        private async Task RefreshCalendar()
+        {
+            try
+            {
+                StateContainer.SetLoadingState(true);
+                _calendarCellItems = await CalendarService.GetCalendarCellItemsAsync(_date.Year, _date.Month);
+            }
+            catch (Exception ex)
+            {
+                StateContainer.SetMessage(ex.Message);
+            }
+            finally
+            {
+                StateContainer.SetLoadingState(false);
+            }
+        }
+    }
+}
