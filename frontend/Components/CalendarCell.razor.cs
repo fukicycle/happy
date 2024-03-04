c56a71662272bc50ed8a5bffc917c9f4d66fb33d
fukicycle <lego.sato.4135@gmail.com>
2024-03-04 16:48:29 +0900

    add calendar cell

---
+++ frontend/Components/CalendarCell.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Happy.frontend.Components
+{
+    public partial class CalendarCell
+    {
+        [Parameter]
+        public DateTime DateTime { get; set; }
+        [Parameter]
+        public EventCallback<DateTime> CellOnClick { get; set; }
+
+        private int? _day = null;
+
+        protected override void OnInitialized()
+        {
+            _day = DateTime.Day;
+        }
+
+        private async Task CellOnClicked()
+        {
+            await CellOnClick.InvokeAsync(DateTime);
+        }
+    }
+}
