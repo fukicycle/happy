1f57dd7e79b4e8a48d201767add39d305678b3ee
fukicycle <lego.sato.4135@gmail.com>
2024-03-04 11:18:48 +0900

    Update goal point response dto

--- Shared/Dto/Response/GoalPointResponseDto.cs
+++ Shared/Dto/Response/GoalPointResponseDto.cs
@@ -8,14 +8,16 @@ namespace Happy.Shared.Dto.Response
 {
     public class GoalPointResponseDto
     {
-        public GoalPointResponseDto(Guid guid, string content, int point)
+        public GoalPointResponseDto(Guid guid, string content, int point, bool isDone)
         {
             Guid = guid;
             Content = content;
             Point = point;
+            IsDone = isDone;
         }
         public Guid Guid { get; }
         public string Content { get; }
         public int Point { get; }
+        public bool IsDone { get; }
     }
 }
