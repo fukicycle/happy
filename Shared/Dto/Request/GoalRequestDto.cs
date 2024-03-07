761a0e835f551a7f260e78dbbb74a222a7939be4
Fuki Sato <lego.sato.4135@gmail.com>
2024-03-07 23:28:32 +0900

    Dto修正

--- Shared/Dto/Request/GoalRequestDto.cs
+++ Shared/Dto/Request/GoalRequestDto.cs
@@ -2,13 +2,11 @@ namespace Happy.Shared.Dto.Request;
 
 public class GoalRequestDto
 {
-    public GoalRequestDto(string content, int point, string email)
+    public GoalRequestDto(string content, int point)
     {
         Content = content;
         Point = point;
-        Email = email;
     }
     public string Content { get; }
     public int Point { get; }
-    public string Email { get; }
 }
